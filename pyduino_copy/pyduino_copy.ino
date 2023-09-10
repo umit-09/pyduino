094d2ef8e3de5221298c9f086ccd31c099a21627
umittadelen <72702832+umit-09@users.noreply.github.com>
2023-09-10 15:31:15 +0200

    Summary

--- pyduino_copy/pyduino_copy.ino
+++ pyduino_copy/pyduino_copy.ino
@@ -1,6 +1,8 @@
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
-#include <TimeLib.h>
+#include <RTClib.h>
+
+DS1302 rtc; // Create an RTC object
 
 LiquidCrystal_I2C lcd(0x27, 20, 4); // Set the LCD address to 0x27 for a 4x20 display
 
@@ -10,33 +12,39 @@ void setup() {
   lcd.backlight(); // Turn on the backlight
   lcd.setCursor(0, 0); // Set the cursor to the top-left corner
   lcd.print("Arduino LCD Demo"); // Display a welcome message
-  setTime(0); // Initialize the Time library (start from epoch)
+
+  rtc.begin(); // Initialize the RTC module
+
+  // Uncomment the following line if you want to set the RTC to the compile time of your sketch
+  // rtc.adjust(DateTime(__DATE__, __TIME__));
 }
 
 void loop() {
   if (Serial.available() > 0) {
     String inputString = Serial.readStringUntil('\n'); // Read a line from the serial port
     inputString.trim(); // Remove leading and trailing whitespace
-    
+
     // Check if the inputString starts with "SD "
     if (inputString.startsWith("SD ")) {
       String timestamp = inputString.substring(3); // Remove the "SD " prefix
-      
+
       // Convert timestamp to a long integer
       long timestampLong = atol(timestamp.c_str());
-      
-      // Set the system time using the received Unix timestamp
-      setTime(timestampLong);
-      
+
+      // Set the RTC time using the received Unix timestamp
+      DateTime newTime = DateTime(timestampLong);
+      rtc.adjust(newTime);
+
       // Create a formatted date and time string
+      DateTime now = rtc.now();
       char datetimeString[20]; // Buffer for date and time string
-      sprintf(datetimeString, "%02d/%02d/%04d %02d:%02d:%02d", month(), day(), year(), hour(), minute(), second());
-      
+      sprintf(datetimeString, "%02d/%02d/%04d %02d:%02d:%02d", now.month(), now.day(), now.year(), now.hour(), now.minute(), now.second());
+
       // Clear the LCD and print the formatted date and time
       lcd.clear();
       lcd.setCursor(0, 0);
       lcd.print(datetimeString);
-      
+
       // Send acknowledgment back to Python
       Serial.println("ACK");
     }
--- test.py
+++ test.py
@@ -7,9 +7,30 @@ def get_time():
     print("GD\n")
     print("response")
 
+def get_timezone_offset():
+    current_time = datetime.datetime.now()
+    utc_time = datetime.datetime.utcnow()
+    
+    # Calculate the timezone offset in seconds
+    timezone_offset_seconds = (current_time - utc_time).total_seconds()
+    
+    # Calculate the offset in hours and minutes
+    hours, remainder = divmod(int(timezone_offset_seconds), 3600)
+    minutes = int(remainder / 60)
+    
+    return hours, minutes
+
 def set_time():
-    timestamp_utc = datetime.datetime.now(datetime.timezone.utc).timestamp()
-    timestamp = str(int(timestamp_utc))  # Convert to integer for seconds since epoch
+    # Get the current timezone offset
+    hours, minutes = get_timezone_offset()
+    
+    # Get the current time and add the offset
+    current_time = datetime.datetime.now()
+    adjusted_time = current_time + datetime.timedelta(hours=hours, minutes=minutes)
+    
+    # Convert the adjusted time to a Unix timestamp
+    timestamp = str(int(adjusted_time.timestamp()))
+    
     print(f"SD {timestamp}")
     arduino.write(str.encode(f"SD {timestamp}"))
     time.sleep(0.5)
