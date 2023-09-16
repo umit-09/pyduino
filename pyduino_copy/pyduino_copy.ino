82796d7f7ea96949a8a9e1861af50a353fcf188e
umittadelen <72702832+umit-09@users.noreply.github.com>
2023-09-16 11:47:57 +0200

    sum

--- pyduino_copy/pyduino_copy.ino
+++ pyduino_copy/pyduino_copy.ino
@@ -2,51 +2,61 @@
 #include <LiquidCrystal_I2C.h>
 #include <RTClib.h>
 
-DS1302 rtc; // Create an RTC object
+DS1302 rtc;  // Create an RTC object
 
-LiquidCrystal_I2C lcd(0x27, 20, 4); // Set the LCD address to 0x27 for a 4x20 display
+LiquidCrystal_I2C lcd(0x27, 20, 4);  // Set the LCD address to 0x27 for a 4x20 display
+
+int day, month, year, hour, minute, second;
 
 void setup() {
-  Serial.begin(9600); // Initialize serial communication
-  lcd.init(); // Initialize the LCD
-  lcd.backlight(); // Turn on the backlight
-  lcd.setCursor(0, 0); // Set the cursor to the top-left corner
-  lcd.print("Arduino LCD Demo"); // Display a welcome message
+  Serial.begin(9600);             // Initialize serial communication
+  lcd.init();                     // Initialize the LCD
+  lcd.backlight();                // Turn on the backlight
+  lcd.setCursor(0, 0);            // Set the cursor to the top-left corner
+  lcd.print("Arduino LCD Demo");  // Display a welcome message
 
-  rtc.begin(); // Initialize the RTC module
-
-  // Uncomment the following line if you want to set the RTC to the compile time of your sketch
-  // rtc.adjust(DateTime(__DATE__, __TIME__));
+  rtc.begin();  // Initialize the RTC module
 }
 
 void loop() {
+  for (int i = 0; i < 1000; i++) {
+    serial_check();
+    delay(1);
+  }
+
+  DateTime now = rtc.now();
+  char buf[100];
+  strncpy(buf, "YYYY.MM.DD hh:mm:ss", 100);
+
+  lcd.clear();
+  lcd.setCursor(0, 1);
+  lcd.print(now.format(buf));
+}
+
+void serial_check() {
   if (Serial.available() > 0) {
-    String inputString = Serial.readStringUntil('\n'); // Read a line from the serial port
-    inputString.trim(); // Remove leading and trailing whitespace
+    String inputString = Serial.readStringUntil('\n');  // Read a line from the serial port
+    inputString.trim();                                 // Remove leading and trailing whitespace
 
     // Check if the inputString starts with "SD "
     if (inputString.startsWith("SD ")) {
-      String timestamp = inputString.substring(3); // Remove the "SD " prefix
+      String datetimeString = inputString.substring(3);  // Remove the "SD " prefix
+      
+      sscanf(datetimeString.c_str(), "%2d/%2d/%4d/%2d/%2d/%2d", &day, &month, &year, &hour, &minute, &second);
 
-      // Convert timestamp to a long integer
-      long timestampLong = atol(timestamp.c_str());
-
-      // Set the RTC time using the received Unix timestamp
-      DateTime newTime = DateTime(timestampLong);
+      // Set the RTC time using the received date and time components
+      DateTime newTime(year, month, day, hour, minute, second);
+      
       rtc.adjust(newTime);
 
-      // Create a formatted date and time string
-      DateTime now = rtc.now();
-      char datetimeString[20]; // Buffer for date and time string
-      sprintf(datetimeString, "%02d/%02d/%04d %02d:%02d:%02d", now.month(), now.day(), now.year(), now.hour(), now.minute(), now.second());
-
-      // Clear the LCD and print the formatted date and time
+      // Clear the LCD and print the received date and time
       lcd.clear();
-      lcd.setCursor(0, 0);
+      lcd.setCursor(0, 1);
       lcd.print(datetimeString);
 
       // Send acknowledgment back to Python
       Serial.println("ACK");
+      delay(2000);
     }
   }
 }
--- test.py
+++ test.py
@@ -1,55 +1,71 @@
-import time, serial, datetime
-from time import gmtime, strftime
+import time
+import serial
+import datetime
+import threading
+import sys
 
-arduino = serial.Serial(port='COM8', baudrate=9600, timeout=.1) 
+arduino = serial.Serial(port='COM8', baudrate=9600, timeout=0)  # Set timeout to 0 for non-blocking read
 
 def get_time():
-    print("GD\n")
-    print("response")
-
-def get_timezone_offset():
-    current_time = datetime.datetime.now()
-    utc_time = datetime.datetime.utcnow()
-    
-    # Calculate the timezone offset in seconds
-    timezone_offset_seconds = (current_time - utc_time).total_seconds()
-    
-    # Calculate the offset in hours and minutes
-    hours, remainder = divmod(int(timezone_offset_seconds), 3600)
-    minutes = int(remainder / 60)
-    
-    return hours, minutes
+    print("\nGetting Time...\n")
+    # Your code to handle getting time from Arduino goes here
 
 def set_time():
-    # Get the current timezone offset
-    hours, minutes = get_timezone_offset()
-    
-    # Get the current time and add the offset
-    current_time = datetime.datetime.now()
-    adjusted_time = current_time + datetime.timedelta(hours=hours, minutes=minutes)
-    
-    # Convert the adjusted time to a Unix timestamp
-    timestamp = str(int(adjusted_time.timestamp()))
-    
-    print(f"SD {timestamp}")
-    arduino.write(str.encode(f"SD {timestamp}"))
+    formatted_time = get_formatted_time()
+    print(f"\nSending: {formatted_time}")
+    arduino.write(formatted_time.encode())
     time.sleep(0.5)
+    
+    # Read the response from Arduino
+    response = arduino.readline().decode().strip()
+    print(f"Received response from Arduino: {response}\n")
+
+def read_serial():
+    try:
+        while not done_flag.is_set():
+            data = arduino.readline().decode().strip()
+            if data:
+                print(f"Received data from Arduino: {data}")
+            time.sleep(0.001)  # Sleep for 1 millisecond (adjust as needed)
+    except serial.SerialException:
+        pass
+
+def get_formatted_time():
+    current_time = datetime.datetime.now()
+    formatted_time = current_time.strftime("SD %d/%m/%Y/%H/%M/%S")
+    return formatted_time
 
 if __name__ == "__main__":
-    while True:
-        print("\nMenu:")
-        print("1. Get Time")
-        print("2. Set Time")
-        print("3. Quit")
-        choice = input("Enter your choice (1/2/3): ")
+    done_flag = threading.Event()  # Flag to signal when to stop reading from the serial port
 
-        if choice == '1':
-            get_time()
-        elif choice == '2':
-            set_time()
-            response = arduino.readline().decode().strip()  # Read acknowledgment from Arduino
-            print(f"Arduino says: {response}")
-        elif choice == '3':
-            break
-        else:
-            print("Invalid choice. Please select 1, 2, or 3.")
+    arduino.flushInput()  # Clear any existing data in the input buffer
+    arduino.flushOutput()  # Clear any existing data in the output buffer
+
+    # Create a separate thread to continuously read from the serial port
+    serial_thread = threading.Thread(target=read_serial)
+    serial_thread.daemon = True
+    serial_thread.start()
+
+    try:
+        while True:
+            print("\nMenu:")
+            print("1. Get Time")
+            print("2. Set Time")
+            print("3. Quit")
+            choice = input("Enter your choice (1/2/3): ")
+
+            if choice == '1':
+                get_time()
+            elif choice == '2':
+                set_time()
+            elif choice == '3':
+                done_flag.set()  # Set the flag to signal the serial reading thread to stop
+                serial_thread.join()  # Wait for the serial reading thread to finish
+                break
+            else:
+                print("Invalid choice. Please select 1, 2, or 3.")
+    except KeyboardInterrupt:
+        done_flag.set()  # Set the flag to signal the serial reading thread to stop
+        serial_thread.join()  # Wait for the serial reading thread to finish
+        arduino.close()  # Close the serial port when done
+        sys.exit(0)
